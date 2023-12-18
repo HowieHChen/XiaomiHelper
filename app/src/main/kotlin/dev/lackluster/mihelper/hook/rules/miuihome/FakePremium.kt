bd9fa0ff2b77ff55b188c1a8e5eaaff23f3455dc
howie <howie.c@outlook.com>
2023-12-18 12:53:14 +0800

    修改 桌面高端设备伪装

--- app/src/main/kotlin/dev/lackluster/mihelper/hook/rules/miuihome/FakePremium.kt
+++ app/src/main/kotlin/dev/lackluster/mihelper/hook/rules/miuihome/FakePremium.kt
@@ -10,147 +10,110 @@ import dev.lackluster.mihelper.utils.Prefs.hasEnable
 object FakePremium :YukiBaseHooker() {
     override fun onHook() {
         hasEnable(PrefKey.HOME_FAKE_PREMIUM) {
-            val deviceLevelUtilsClass = "com.miui.home.launcher.common.DeviceLevelUtils".toClass()
-            val deviceConfigClass = "com.miui.home.launcher.DeviceConfig".toClass()
-            try {
-                if ("com.miui.home.launcher.common.CpuLevelUtils".hasClass()) {
-                    "com.miui.home.launcher.common.CpuLevelUtils".toClass()
-                        .method {
-                            name = "getQualcommCpuLevel"
-                            paramCount = 1
-                        }.ignored().onNoSuchMethod {
-                            throw it
-                        }
-                        .hook {
-                            replaceTo(2)
-                        }
+            val deviceLevelUtilsClass = "com.miui.home.launcher.common.DeviceLevelUtils".toClassOrNull()
+            val deviceConfigClass = "com.miui.home.launcher.DeviceConfig".toClassOrNull()
+            "com.miui.home.launcher.common.CpuLevelUtils".toClassOrNull()
+                ?.method {
+                    name = "getQualcommCpuLevel"
+                    paramCount = 1
+                }?.ignored()
+                ?.hook {
+                    replaceTo(2)
                 }
-            } catch (e: Exception) {
-                if ("miuix.animation.utils.DeviceUtils".hasClass()) {
-                    "miuix.animation.utils.DeviceUtils".toClass()
-                        .method {
-                            name = "getQualcommCpuLevel"
-                            paramCount = 1
-                        }
-                        .hook {
-                            replaceTo(2)
-                        }
+            "miuix.animation.utils.DeviceUtils".toClassOrNull()
+                ?.method {
+                    name = "getQualcommCpuLevel"
+                    paramCount = 1
+                }?.ignored()
+                ?.hook {
+                    replaceTo(2)
                 }
-            }
-            runCatching {
-                deviceConfigClass.method {
+            deviceLevelUtilsClass?.apply {
+                method {
                     name = "isUseSimpleAnim"
                 }.ignored().hook {
                     replaceToFalse()
                 }
-            }
-            runCatching {
-                deviceLevelUtilsClass.method {
+                method {
                     name = "getDeviceLevel"
                 }.ignored().hook {
                     replaceTo(2)
                 }
-            }
-            runCatching {
-                deviceLevelUtilsClass.method {
+                method {
                     name = "getDeviceLevelOfCpuAndGpu"
                 }.ignored().hook {
                     replaceTo(2)
                 }
-            }
-            runCatching {
-                deviceConfigClass.method {
-                    name = "isSupportCompleteAnimation"
-                }.ignored().hook {
-                    replaceToTrue()
-                }
-            }
-
-            runCatching {
-                deviceLevelUtilsClass.method {
+                method {
                     name = "isLowLevelOrLiteDevice"
                 }.ignored().hook {
                     replaceToFalse()
                 }
             }
-
-            runCatching {
-                deviceConfigClass.method {
+            deviceConfigClass?.apply {
+                method {
+                    name = "isSupportCompleteAnimation"
+                }.ignored().hook {
+                    replaceToTrue()
+                }
+                method {
                     name = "isMiuiLiteVersion"
                 }.ignored().hook {
                     replaceToFalse()
                 }
-            }
 
-            runCatching {
-                "com.miui.home.launcher.util.noword.NoWordSettingHelperKt".toClass()
-                    .method {
-                        name = "isNoWordAvailable"
-                    }.ignored()
-                    .hook {
-                        replaceToTrue()
-                    }
             }
-
-            runCatching {
-                "android.os.SystemProperties".toClass()
-                    .method {
-                        name = "getBoolean"
-                        param(StringClass, BooleanType)
-                    }.ignored().hookAll {
-                        before {
-                            if (this.args(0).string() == "ro.config.low_ram.threshold_gb") {
-                                this.result = false
-                            }
-                            if (this.args(0).string() == "ro.miui.backdrop_sampling_enabled") {
-                                this.result = true
-                            }
+            "com.miui.home.launcher.util.noword.NoWordSettingHelperKt".toClassOrNull()
+                ?.method {
+                    name = "isNoWordAvailable"
+                }?.ignored()
+                ?.hook {
+                    replaceToTrue()
+                }
+            "android.os.SystemProperties".toClassOrNull()
+                ?.method {
+                    name = "getBoolean"
+                    param(StringClass, BooleanType)
+                }?.ignored()
+                ?.hookAll {
+                    before {
+                        if (this.args(0).string() == "ro.config.low_ram.threshold_gb") {
+                            this.result = false
+                        }
+                        if (this.args(0).string() == "ro.miui.backdrop_sampling_enabled") {
+                            this.result = true
                         }
                     }
-            }
-
-            runCatching {
-                "com.miui.home.launcher.common.Utilities".toClass()
-                    .method {
-                        name = "canLockTaskView"
-                    }.ignored()
-                    .hook {
-                        replaceToTrue()
-                    }
-            }
-
-            runCatching {
-                "com.miui.home.launcher.MIUIWidgetUtil".toClass()
-                    .method {
-                        name = "isMIUIWidgetSupport"
-                    }.ignored()
-                    .hook {
-                        replaceToTrue()
-                    }
-            }
-
-            runCatching {
-                "com.miui.home.launcher.MiuiHomeLog".toClass()
-                    .method {
-                        name = "log"
-                        param(StringClass, StringClass)
-                    }.ignored()
-                    .hook {
-                        before {
-                            this.result = null
-                        }
-                    }
-            }
-
-            runCatching {
-                "com.xiaomi.onetrack.OneTrack".toClass()
-                    .method {
-                        name = "isDisable"
-                    }.ignored()
-                    .hook {
-                        replaceToTrue()
-                    }
-            }
+                }
+            "com.miui.home.launcher.common.Utilities".toClassOrNull()
+                ?.method {
+                    name = "canLockTaskView"
+                }?.ignored()
+                ?.hook {
+                    replaceToTrue()
+                }
+            "com.miui.home.launcher.MIUIWidgetUtil".toClassOrNull()
+                ?.method {
+                    name = "isMIUIWidgetSupport"
+                }?.ignored()
+                ?.hook {
+                    replaceToTrue()
+                }
+            "com.miui.home.launcher.MiuiHomeLog".toClassOrNull()
+                ?.method {
+                    name = "log"
+                    param(StringClass, StringClass)
+                }?.ignored()
+                ?.hook {
+                    intercept()
+                }
+            "com.xiaomi.onetrack.OneTrack".toClassOrNull()
+                ?.method {
+                    name = "isDisable"
+                }?.ignored()
+                ?.hook {
+                    replaceToTrue()
+                }
         }
     }
 }
