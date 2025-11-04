2440b39f553cd14038d3a061b2a6a99a01098b09
howie <howie.c@outlook.com>
2025-11-05 00:55:03 +0800

    fix: [SystemUI] Custom network speed font weight setting not working

--- app/src/main/kotlin/dev/lackluster/mihelper/hook/rules/systemui/statusbar/NetworkSpeed.kt
+++ app/src/main/kotlin/dev/lackluster/mihelper/hook/rules/systemui/statusbar/NetworkSpeed.kt
@@ -1,3 +1,23 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This file is part of XiaomiHelper project
+ * Copyright (C) 2025 HowieHChen, howie.dev@outlook.com
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.lackluster.mihelper.hook.rules.systemui.statusbar
 
 import android.graphics.Typeface
@@ -19,7 +39,6 @@ import dev.lackluster.mihelper.hook.rules.systemui.ResourcesUtils.TextAppearance
 import dev.lackluster.mihelper.hook.rules.systemui.compat.CommonClassUtils.fontPath
 import dev.lackluster.mihelper.utils.Prefs
 import dev.lackluster.mihelper.utils.factory.dpFloat
-
 
 object NetworkSpeed : YukiBaseHooker() {
     private const val KEY_UP_BYTES = "KEY_UP_BYTES"
@@ -45,13 +64,13 @@ object NetworkSpeed : YukiBaseHooker() {
     private val modifyNetSpeedSeparateFW =
         mode != 0 && Prefs.getBoolean(FontWeight.NET_SPEED_SEPARATE, false) && valueNetSeparateFW in 1..1000
     private val typefaceNetSpeedNumberFW by lazy {
-        Typeface.Builder(fontPath).setFontVariationSettings("'wght' $modifyNetSpeedNumberFW").build()
+        Typeface.Builder(fontPath).setFontVariationSettings("'wght' $valueNetSpeedFW").build()
     }
     private val typefaceNetSpeedUnitFW by lazy {
-        Typeface.Builder(fontPath).setFontVariationSettings("'wght' $modifyNetSpeedUnitFW").build()
+        Typeface.Builder(fontPath).setFontVariationSettings("'wght' $valueNetUnitFW").build()
     }
     private val typefaceNetSpeedSeparateFW by lazy {
-        Typeface.Builder(fontPath).setFontVariationSettings("'wght' $modifyNetSpeedSeparateFW").build()
+        Typeface.Builder(fontPath).setFontVariationSettings("'wght' $valueNetSeparateFW").build()
     }
 
     override fun onHook() {
